52a715e81a04ac929b17137b85b92dce296403d6
MarioT573 <mariotrig@yahoo.ca>
2023-03-28 19:17:32 -0400

    added react file structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Navbar";
+import SignUp from "./components/form";
+
+function App() {
+  return (
+  <>
+    <Header />
+    <SignUp />
+  </>);
+}
+
+export default App;
