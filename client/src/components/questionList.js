8cfac9d7f029ab4b5632c1ff9f1328fc5c8e901e
kanhairaval <kanhairaval@gmail.com>
2023-04-02 11:32:32 -0400

    refactored handlesubmit

--- client/src/components/questionList.js
+++ client/src/components/questionList.js
@@ -1,101 +1,95 @@
 import React, { useState } from "react";
-import './css/questionList.css';
 
-let questionData = [
-    {
-        title: "Question 1",
-        description: "How many Kilowatt-hours (kWh) of electricty do you use in a month?",
-        sample: "(e.g. 300)",
-        type: "kilowattConsumption",
-    },
-    {
-        title: "Question 2",
-        description: "How much gas, in Litres (L), do you use in a month? ",
-        sample: "(e.g. 125)",
-        type: "consumptionType",
-    },
-    {
-        title: "Question 3",
-        description: "How many kilograms (kg) of meat do you consume in a month?",
-        sample: "(e.g. 7)",
-        type: "fuelAmount",
-    }
-]
+const questionData = [
+  {
+    title: "Question 1",
+    description: "How many Kilowatt-hours (kWh) of electricity do you use in a month?",
+    sample: "(e.g. 300)",
+    type: "kilowattConsumption",
+  },
+  {
+    title: "Question 2",
+    description: "How much gas, in Litres (L), do you use in a month? ",
+    sample: "(e.g. 125)",
+    type: "consumptionType",
+  },
+  {
+    title: "Question 3",
+    description: "How many kilograms (kg) of meat do you consume in a month?",
+    sample: "(e.g. 7)",
+    type: "fuelAmount",
+  },
+];
 
 function Questions() {
-    const [count, setCount] = useState(0);
-    const [kilowattConsumption, setKilowattConsumption] = useState("");
-    const [consumptionType, setConsumptionType] = useState("");
-    const [fuelAmount, setFuelAmount] = useState("");
-    let questionNow = questionData[count];
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [kilowattConsumption, setKilowattConsumption] = useState("");
+  const [consumptionType, setConsumptionType] = useState("");
+  const [fuelAmount, setFuelAmount] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const currentQuestion = questionData[questionIndex];
 
-        const encodedParams = new URLSearchParams();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        if (questionNow.type === "kilowattConsumption") {
-            encodedParams.append("kWh", kilowattConsumption);
-            setKilowattConsumption("");
-        } else if (questionNow.type === "consumptionType") {
-            encodedParams.append("type", consumptionType);
-            setConsumptionType("");
-        } else if (questionNow.type === "fuelAmount") {
-            encodedParams.append("litres", fuelAmount);
-            setFuelAmount("");
-        }
+    let response;
 
-        console.log(encodedParams);
-        const options = {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'X-RapidAPI-Key': '9c4f117a05mshc12c5f4b819c371p1f3d05jsnb17df03aee16',
-                'X-RapidAPI-Host': 'tracker-for-carbon-footprint-api.p.rapidapi.com'
-            },
-            body: encodedParams
-        };
+    if (currentQuestion.type === "kilowattConsumption") {
+      response = await fetch(`https://tracker-for-carbon-footprint-api.p.rapidapi.com/electricityToCO2e?kWh=${kilowattConsumption}`, {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "9c4f117a05mshc12c5f4b819c371p1f3d05jsnb17df03aee16",
+          "X-RapidAPI-Host": "tracker-for-carbon-footprint-api.p.rapidapi.com",
+        },
+      });
+    } else if (currentQuestion.type === "consumptionType") {
+      response = await fetch(`https://tracker-for-carbon-footprint-api.p.rapidapi.com/fuelToCO2e?type=${consumptionType}`, {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "9c4f117a05mshc12c5f4b819c371p1f3d05jsnb17df03aee16",
+          "X-RapidAPI-Host": "tracker-for-carbon-footprint-api.p.rapidapi.com",
+        },
+      });
+    } else if (currentQuestion.type === "fuelAmount") {
+      response = await fetch(`https://tracker-for-carbon-footprint-api.p.rapidapi.com/meatToCO2e?kg=${fuelAmount}`, {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "9c4f117a05mshc12c5f4b819c371p1f3d05jsnb17df03aee16",
+          "X-RapidAPI-Host": "tracker-for-carbon-footprint-api.p.rapidapi.com",
+        },
+      });
+    }
 
-        fetch('https://tracker-for-carbon-footprint-api.p.rapidapi.com/fuelToCO2e', options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
-        
-        if (count === questionData.length - 1) {
-            setCount(0);
-        } else {
-            setCount(count + 1);
-        }
-    };
+    const data = await response.json();
+    console.log(data);
 
-    return (
-        <form className="questionForm" onSubmit={handleSubmit}>
-        <span className="questionTitle">{questionNow.title}</span>
-        <p className="questionDescription">{questionNow.description}</p>
-        <div>
-            <input placeholder={questionNow.sample} type="text" name={questionNow.type} id={questionNow.type} 
-            value={
-                questionNow.type === "kilowattConsumption"
-                ? kilowattConsumption
-                : questionNow.type === "consumptionType"
-                ? consumptionType
-                : questionNow.type === "fuelAmount"
-                ? fuelAmount
-                : ""       
-            } 
-            onChange={(event) => 
-                questionNow.type === "kilowattConsumption"
-                ? setKilowattConsumption(event.target.value)
-                : questionNow.type === "consumptionType"
-                ? setConsumptionType(event.target.value)
-                : questionNow.type === "fuelAmount"
-                ? setFuelAmount(event.target.value)
-                : ""
-            } />
+    // If we're on the last question, reset back to the first question
+    if (questionIndex === questionData.length - 1) {
+      setQuestionIndex(0);
+    } else {
+      setQuestionIndex(questionIndex + 1);
+    }
+  };
 
+  return (
+    <div>
+      {/* Display the current question */}
+      <h1>{currentQuestion.title}</h1>
+      <p>{currentQuestion.description}</p>
+        <p>{currentQuestion.sample}</p>
+        <form onSubmit={handleSubmit}>
+            {currentQuestion.type === "kilowattConsumption" && (
+                <input type="text" value={kilowattConsumption} onChange={(event) => setKilowattConsumption(event.target.value)} />
+            )}
+            {currentQuestion.type === "consumptionType" && (
+                <input type="text" value={consumptionType} onChange={(event) => setConsumptionType(event.target.value)} />
+            )}
+            {currentQuestion.type === "fuelAmount" && (
+                <input type="text" value={fuelAmount} onChange={(event) => setFuelAmount(event.target.value)} />
+            )}
             <button type="submit">Submit</button>
-        </div>
-    </form>
+        </form>
+    </div>
     );
 }
 
