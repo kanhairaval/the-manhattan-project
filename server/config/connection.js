e8a1530b03c94df6e61087a0c4249cfe3fb100dc
kanhairaval <kanhairaval@gmail.com>
2023-04-02 10:40:57 -0400

    upto date

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/themanhattanproject', 
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/themanhattanproject',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
