402721d504a43a8265bf48b82a48008e081ea73b
MarioT573 <mariotrig@yahoo.ca>
2023-04-03 17:05:01 -0400

    profile page styling

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,16 +9,62 @@ const ProfilePage = ({ user }) => {
     if (loading) {
         return <div>Loading...</div>;
     }
-        
+        console.log(currentUser);
     return (
-        <div className="profile-container">
-            <div className="profile-info">
-                <div className="profile-name">{currentUser.name}</div>
-                <div className="profile-email">{currentUser.email}</div>
-                <div className="profile-score">Score: {currentUser.score}</div>
+        <div class="profile-container">
+            <div class="main-body">
+            <div class="row gutters-sm">
+                <div class="col-md-4 mb-3">
+                    <div class="card profileCard">
+                        <div class="card-body">
+                            <div class="d-flex flex-column align-items-center text-center">
+                                <img src="https://www.freeiconspng.com/uploads/account-profile-user-icon--icon-search-engine-10.png" alt="account icon" width="150" />
+                                <div class="mt-3">
+                                    <h4>{currentUser.name}</h4>
+                                    <p class="text-grey mb-1">Environment Protector </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="card mb-3">
+                        <div class="card-body accountCard">
+
+                            <div class="row accountInfo">
+                                <div class="col-sm-3">
+                                    <h3 class="mb-0">Account Information</h3>
+                                </div>
+                            </div>
+
+                            <hr></hr>
+
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Full Name</h6>
+                                </div>
+                                <div class="col-sm-9 text-grey">
+                                    {currentUser.name}
+                                </div>
+                            </div>
+
+                            <hr></hr>
+
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Email</h6>
+                                </div>
+                                <div class="col-sm-9 text-grey">
+                                {currentUser.email}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
       );
-    };
+};
   
   export default ProfilePage;
