e6bd0287c720283cc09ae9e224cd7e5403aa4f46
semir <seper56@gmail.com>
2023-04-02 21:25:30 -0400

    ..

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,7 +19,7 @@ function HomePage() {
               <div/>
                       <h3 className="main-hander">Earthify</h3>
                       <div className="underlne mx auto"></div>
-                      <p> ya mum</p>
+                      <p>An environment calculator is a tool that enables individuals and businesses to measure their environmental impact. It allows users to input data such as energy consumption, waste generation, and transportation emissions, and then calculates the carbon footprint and other environmental metrics associated with these activities. This information can be used to identify areas where improvements can be made to reduce environmental impact and promote sustainability. The environment calculator is an important tool in the fight against climate change and the promotion of eco-friendly practices.</p>
                       <Link to="/about" className="btn btn-warning shadow">Read More</Link>
                       <div>
                       </div>
