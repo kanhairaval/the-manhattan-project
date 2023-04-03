c4c37f6dba07de7833fd033dc7786352de6a7298
MarioT573 <mariotrig@yahoo.ca>
2023-04-03 17:19:45 -0400

    profile page styling

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -11,50 +11,50 @@ const ProfilePage = ({ user }) => {
     }
         console.log(currentUser);
     return (
-        <div class="profile-container">
-            <div class="main-body">
-            <div class="row gutters-sm">
-                <div class="col-md-4 mb-3">
-                    <div class="card profileCard">
-                        <div class="card-body">
-                            <div class="d-flex flex-column align-items-center text-center">
-                                <img src="https://www.freeiconspng.com/uploads/account-profile-user-icon--icon-search-engine-10.png" alt="account icon" width="150" />
-                                <div class="mt-3">
+        <div className="profile-container">
+            <div className="main-body">
+            <div className="profile row gutters-sm">
+                <div className="profileAlign col-md-4 mb-3">
+                    <div className="card profileCard">
+                        <div className="card-body">
+                            <div className="d-flex flex-column align-items-center text-center">
+                                <img src={require('../images/profileicon.png')} alt="account icon" width="150" />
+                                <div className="mt-3">
                                     <h4>{currentUser.name}</h4>
-                                    <p class="text-grey mb-1">Environment Protector </p>
+                                    <p className="text-grey mb-1">Environment Protector </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div class="card mb-3">
-                        <div class="card-body accountCard">
+                <div className='account'>
+                    <div className="card mb-3">
+                        <div className="card-body accountCard">
 
-                            <div class="row accountInfo">
-                                <div class="col-sm-3">
-                                    <h3 class="mb-0">Account Information</h3>
+                            <div className="row">
+                                <div className="col-sm-3">
+                                    <h3 className="mb-0">Account Information</h3>
                                 </div>
                             </div>
 
                             <hr></hr>
 
-                            <div class="row">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Full Name</h6>
+                            <div className="row">
+                                <div className="col-sm-3">
+                                    <h6 className="mb-0">Full Name</h6>
                                 </div>
-                                <div class="col-sm-9 text-grey">
+                                <div className="col-sm-9 text-grey">
                                     {currentUser.name}
                                 </div>
                             </div>
 
                             <hr></hr>
 
-                            <div class="row">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Email</h6>
+                            <div className="row">
+                                <div className="col-sm-3">
+                                    <h6 className="mb-0">Email</h6>
                                 </div>
-                                <div class="col-sm-9 text-grey">
+                                <div className="col-sm-9 text-grey">
                                 {currentUser.email}
                                 </div>
                             </div>
